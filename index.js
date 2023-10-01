fa6201271447ecf4d119350b32f9ce74426e1d58
davidEsparza7902 <desparzas7902@gmail.com>
2023-09-30 22:23:12 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-const app = express()
-// In order to get the body content of the request
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(cors())
+const Note = require('./models/note')
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
+const app = express()
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError')
+    return response.status(400).json({ error: error.message })
+  next(error)
+}
+
 const requestLogger = (req, res, next) => {
   console.log('Method:', req.method)
   console.log('Path:  ', req.path)
@@ -30,59 +21,89 @@ const requestLogger = (req, res, next) => {
   console.log('---')
   next()
 }
-app.use(requestLogger)
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((note) => note.id)) : 0
-  return maxId + 1
-}
-
-// Test
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>')
-})
-
-// GET all Notes
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
-// GET a Note
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find((n) => n.id === id)
-  if (note) res.json(note)
-  else res.status(404).end()
-})
-
-// DELETE a Note
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter((note) => note.id !== id)
-  response.status(204).end()
-})
-
-// CREATE a Note
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing :P',
-    })
-  }
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
-  notes = notes.concat(note)
-  response.json(note)
-})
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
-app.use(unknownEndpoint)
-const PORT = process.env.PORT || 3001
+
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
+app.use(requestLogger)
+app.use(errorHandler)
+
+// GET all Notes
+app.get('/api/notes', async (req, res) => {
+  try {
+    const notes = await Note.find({})
+    res.json(notes)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+// GET a Note
+app.get('/api/notes/:id', async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id)
+    if (note) res.json(note)
+    else res.status(404).end()
+  } catch (e) {
+    next(e)
+  }
+})
+
+// CREATE a Note
+app.post('/api/notes', async (request, response, next) => {
+  try {
+    const body = request.body
+
+    if (body.content === undefined) {
+      return response.status(400).json({
+        error: 'content missing :P',
+      })
+    }
+
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+
+    const savedNote = await note.save()
+    response.json(savedNote)
+  } catch (error) {
+    next(error)
+    /* response.status(500).json({ error: 'Internal Server Error' }) */
+  }
+})
+
+// DELETE a Note
+app.delete('/api/notes/:id', async (request, response, next) => {
+  try {
+    const note = await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (e) {
+    next(e)
+  }
+})
+
+// EDIT a note
+app.put('/api/notes/:id', async (request, response, next) => {
+  try {
+    const { content, important } = request.body
+
+    const updatedNote = await Note.findByIdAndUpdate(
+      request.params.id,
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+    )
+    response.json(updatedNote)
+  } catch (error) {
+    next(error)
+  }
+})
+
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
